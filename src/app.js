4b5ab44970c8707865e06c3f4efe64b233efda52
mabdullahse <mabdullah.se@gmail.com>
2023-04-30 10:03:50 +0500

    auth project completed

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { AdminPage } from "./pages/admin-page";
+import { HomePage } from "./pages/home-page";
+import { NotFoundPage } from "./pages/not-found-page";
+import { ProfilePage } from "./pages/profile-page";
+import { ProtectedPage } from "./pages/protected-page";
+import { PublicPage } from "./pages/public-page";
+import { AuthenticationGuard } from "./components/authentication-guard";
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/profile"
+        element={<AuthenticationGuard component={ProfilePage} />}
+      />
+      <Route path="/public" element={<PublicPage />} />
+      <Route
+        path="/protected"
+        element={<AuthenticationGuard component={ProtectedPage} />}
+      />
+      <Route
+        path="/admin"
+        element={<AuthenticationGuard component={AdminPage} />}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
